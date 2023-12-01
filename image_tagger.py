cdaebb20852b0ce8193e2ce00d00adc728de0acf
Shirish <shirishaddaganti@gmail.com>
2023-12-01 15:40:54 -0500

    gpt vision responses

--- image_tagger.py
+++ image_tagger.py
@@ -5,6 +5,7 @@ import requests
 import json
 from keys import keys
 from prompts import tagger
+import time
 
 # Function to encode a single image
 def encode_image(image_path):
@@ -43,69 +44,69 @@ def save_response_content(image_name, content):
 # Folder path containing images
 folder_path = "new_dataset"
 
+count = 0
 # Iterate over images in the folder
-count=0
 for image_filename in os.listdir(folder_path):
-    if count==2:
-        break
-    if image_filename.endswith(('.jpg', '.jpeg', '.png')):
-        image_path = os.path.join(folder_path, image_filename)
-        base64_image = encode_image(image_path)
+    if count > 77 :
+        if image_filename.endswith(('.jpg', '.jpeg', '.png')):
+            image_path = os.path.join(folder_path, image_filename)
+            base64_image = encode_image(image_path)
 
-        # Prepare the image content for payload
-        image_content = {
-            "type": "image_url",
-            "image_url": {
-                "url": f"data:image/jpeg;base64,{base64_image}"
-            }
-        }
-
-        # The text content remains the same
-        text_content = {
-            "type": "text",
-            "text": tagger
-        }
-
-        # Combine the text content with the image contents
-        combined_contents = [text_content] + [image_content]
-
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {openai.api_key}"
-        }
-
-        payload = {
-            "model": "gpt-4-vision-preview",
-            "messages": [
-                {
-                    "role": "user",
-                    "content": combined_contents
+            # Prepare the image content for payload
+            image_content = {
+                "type": "image_url",
+                "image_url": {
+                    "url": f"data:image/jpeg;base64,{base64_image}"
                 }
-            ],
-            "max_tokens": 4000
-        }
+            }
 
-        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+            # The text content remains the same
+            text_content = {
+                "type": "text",
+                "text": tagger
+            }
 
-        # Check if the response was successful
-        if response.status_code == 200:
-            response_json = response.json()
-            try:
-                content = response_json['choices'][0]['message']['content']
-                print(content)
+            # Combine the text content with the image contents
+            combined_contents = [text_content] + [image_content]
 
-                # Save the 'content' part of the response along with the image name
-                save_response_content(image_filename, content)
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {openai.api_key}"
+            }
 
-                print(f"Response content for {image_filename} saved to '{filename}'.")
+            payload = {
+                "model": "gpt-4-vision-preview",
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": combined_contents
+                    }
+                ],
+                "max_tokens": 4000
+            }
 
-            except KeyError:
-                print("The 'choices' key is missing in the response. Full response:")
-                print(response_json)
-        else:
-            print(f"Failed to get a successful response for {image_filename}. Status code: {response.status_code}")
-            print("Full response:")
-            print(response.text)
+            response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
 
-        count+=1
+            # Check if the response was successful
+            if response.status_code == 200:
+                response_json = response.json()
+                try:
+                    content = response_json['choices'][0]['message']['content']
+                    print(content)
 
+                    # Save the 'content' part of the response along with the image name
+                    save_response_content(image_filename, content)
+
+                    print(f"Response content for {image_filename} saved to '{filename}'.")
+
+                except KeyError:
+                    print("The 'choices' key is missing in the response. Full response:")
+                    print(response_json)
+            else:
+                print(f"Failed to get a successful response for {image_filename}. Status code: {response.status_code}")
+                print("Full response:")
+                print(response.text)
+
+            # Add a sleep duration (e.g., 10 seconds) to avoid rate limits
+            time.sleep(45)  # Provide the number of seconds to sleep
+    count = count +1 
