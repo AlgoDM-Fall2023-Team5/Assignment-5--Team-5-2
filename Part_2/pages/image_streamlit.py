8a180dd85ed1b25a717a1195537cb6d98024dbf9
Shirish <shirishaddaganti@gmail.com>
2023-12-05 18:42:53 -0500

    .

--- Part_2/pages/image_streamlit.py
+++ Part_2/pages/image_streamlit.py
@@ -7,6 +7,7 @@ import json  # Add this import
 import snowflake.connector
 import os
 from PIL import Image
+import boto3
 
 
 
@@ -14,7 +15,7 @@ from image_fetcher import search_and_display_images
 from image_to_image import get_annotations_for_images
 
 
-image_encoder = SentenceTransformer('clip-ViT-B-32')
+#image_encoder = SentenceTransformer('clip-ViT-B-32')
 
 st.title("Image Similarity Search")
 
@@ -52,7 +53,7 @@ if uploaded_image is not None:
         st.image(uploaded_image, caption="Uploaded Image.", use_column_width=True)
 
         # Encode the image
-        image_features = encode_image(uploaded_image, image_encoder)
+        image_features = encode_image(uploaded_image, SentenceTransformer('clip-ViT-B-32'))
 
         # Convert the NumPy array to a list
         image_features_list = image_features.tolist()
@@ -69,37 +70,52 @@ if uploaded_image is not None:
 
 
 
-        image_folder_path = "images"
+        # image_folder_path = "images"
 
 
-        image_files = [f for f in os.listdir(image_folder_path) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+        # image_files = [f for f in os.listdir(image_folder_path) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
 
-        # Output folder for converted images
-        output_folder_path = "/path/to/your/converted/images"
+        # # Output folder for converted images
+        # output_folder_path = "/path/to/your/converted/images"
 
-        # Create the output folder if it doesn't exist
-        os.makedirs(output_folder_path, exist_ok=True)
+        # # Create the output folder if it doesn't exist
+        # os.makedirs(output_folder_path, exist_ok=True)
 
-        st.header("Similar Images")
-        for image_file in image_files:
-            # Create the full path to the input image file
-            input_image_path = os.path.join(image_folder_path, image_file)
+        # st.header("Similar Images")
+        # for image_file in image_files:
+        #     # Create the full path to the input image file
+        #     input_image_path = os.path.join(image_folder_path, image_file)
 
-            # Determine the output image file path with the new format (JPEG)
-            output_image_file = os.path.splitext(image_file)[0] + ".jpeg"
-            output_image_path = os.path.join(output_folder_path, output_image_file)
+        #     # Determine the output image file path with the new format (JPEG)
+        #     output_image_file = os.path.splitext(image_file)[0] + ".jpeg"
+        #     output_image_path = os.path.join(output_folder_path, output_image_file)
 
-            # Open the input image
-            image = Image.open(input_image_path)
+        #     # Open the input image
+        #     image = Image.open(input_image_path)
 
-            # Save the image in JPEG format
-            image.save(output_image_path, "JPEG")
+        #     # Save the image in JPEG format
+        #     image.save(output_image_path, "JPEG")
 
-            # Close the image
-            image.close()
+        #     # Close the image
+        #     image.close()
 
-            # Display the converted image using Streamlit
-            st.image(output_image_path, caption=output_image_file, use_column_width=True)
+        #     # Display the converted image using Streamlit
+        #     st.image(output_image_path, caption=output_image_file, use_column_width=True)
+        s3_client = boto3.client(service_name = 's3',
+                aws_access_key_id='AKIASGVOLPG2BPBLER6T',
+                aws_secret_access_key='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',
+                region_name = 'us-east-2')
+        
+
+        for i in closest_image_ids:
+            img = s3_client.get_object(Bucket='assignment4admt5', Key=f'dataset/{i}')
+            
+
+
+            image_bytes = img['Body'].read()
+
+            # Display the image in Streamlit
+            st.image(image_bytes)
 
 
 
